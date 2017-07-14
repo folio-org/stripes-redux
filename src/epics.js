55e80e5234c5231ade7e40118d479051e6bec164
Michal Kuklis <michal.kuklis@gmail.com>
2017-07-14 18:09:23 -0400

    Add filter operator

--- src/epics.js
+++ src/epics.js
@@ -1,8 +1,10 @@
 import { combineEpics, createEpicMiddleware } from 'redux-observable';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+
 import 'rxjs/add/operator/mergeMap';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/filter';
 
 const epic$ = new BehaviorSubject(combineEpics());
 const rootEpic = (action$, store) =>
