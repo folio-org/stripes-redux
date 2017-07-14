2cb9ef522f7e1aa3c25866f83447a8fb72e3b5e3
Michal Kuklis <michal.kuklis@gmail.com>
2017-07-14 18:17:57 -0400

    Cleanup

--- src/epics.js
+++ src/epics.js
@@ -1,10 +1,6 @@
 import { combineEpics, createEpicMiddleware } from 'redux-observable';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
-
-import 'rxjs/add/operator/mergeMap';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/debounceTime';
-import 'rxjs/add/operator/filter';
+import './operators';
 
 const epic$ = new BehaviorSubject(combineEpics());
 const rootEpic = (action$, store) =>
---
+++ src/operators.js
@@ -0,0 +1,4 @@
+import 'rxjs/add/operator/mergeMap';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/filter';
